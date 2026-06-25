a7bdd719ee3a64b9a4f65f25f8afb4b97376b2a5
aiyalcin <adam.i.yalcin@gmail.com>
2026-06-25 17:31:10 +0200

    Revert "Merge pull request #44 from aiyalcin/Animations"
    This reverts commit 782dd2506c5e9e457c605965ee7a2464249cdf17, reversing
    changes made to 51f5dca84920e01eddffc68149602cde4abf133c.

--- 2.4-UNITY-GROEP/Assets/Scrips/MLAgentScript.cs
+++ 2.4-UNITY-GROEP/Assets/Scrips/MLAgentScript.cs
@@ -45,7 +45,6 @@ public class MLAgentScript : Agent
         SearchingForBox,
         CarryingBox
     }
-
     public State currentState = State.SearchingForBox; // Track the current state of the agent
     private GameObject heldProduct;
     private BoxObject boxObject; // ScriptableObject containing box type and dropoff mapping
@@ -362,4 +361,4 @@ public class MLAgentScript : Agent
         // Destination point: The active target position based on state
         targetLine.SetPosition(1, GetActiveTargetPosition());
     }
-} 
+}
